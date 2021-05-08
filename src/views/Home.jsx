443c07b8fd301ec4741d4b91f6b2b79efe758d5a
Neosoulink <neodarksoulink@gmail.com>
2021-05-08 18:33:58 +0100

    Create & implement header

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header';
+
+// COMPONENTS
 
 export default function HomeView() {
 	return (
 		<div>
+			<Header />
 			Home view
 		</div>
 	)
