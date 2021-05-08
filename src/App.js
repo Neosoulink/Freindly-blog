b97f7155098a9c2da403d7209db4050016d6252c
Neosoulink <neodarksoulink@gmail.com>
2021-05-08 17:29:52 +0100

    Add styled component & react router

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-					<div class="flex-shrink-0">
-						<img class="h-12 w-12" src="https://tailwindcss.com/img/erin-lindford.jpg" alt="ChitChat Logo" />
+				<div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+					<div className="flex-shrink-0">
+						<img className="h-12 w-12" src="https://tailwindcss.com/img/erin-lindford.jpg" alt="ChitChat Logo" />
 					</div>
 					<div>
-						<div class="text-xl font-medium text-black">ChitChat</div>
-						<p class="text-gray-500">You have a new message!</p>
+						<div className="text-xl font-medium text-black">ChitChat</div>
+						<p className="text-gray-500">You have a new message!</p>
 					</div>
 				</div>
 			</header>
