2a542778ff5c27196d521fbf67834b1a67b764c3
Neosoulink <neodarksoulink@gmail.com>
2021-05-08 17:52:52 +0100

    Routes & nested routes managed

--- src/router/index.js
+++ src/router/index.js
@@ -7,23 +7,42 @@ import {
 
 // SCREENS
 function Home() {
-	return <h2>Home</h2>;
+	return <h2>Home Home</h2>;
 }
 
 function About() {
 	return <h2>About</h2>;
 }
 
+const routes = [
+	{
+		path: "/",
+		component: Home
+	},
+	{
+		path: "/about",
+		component: About
+	}
+];
+
+function RouteWithSubRoutes(route) {
+	return (
+		<Route
+			path={route.path}
+			render={props => (
+				<route.component {...props} routes={route.routes} />
+			)}
+		/>
+	);
+}
+
 export default function Router() {
 	return (
 		<BrowserRouter>
 			<Switch>
-				<Route path="/about">
-					<About />
-				</Route>
-				<Route path="/">
-					<Home />
-				</Route>
+				{routes.map((route, i) => (
+					<RouteWithSubRoutes key={i} {...route} />
+				))}
 			</Switch>
 		</BrowserRouter>
 	);
